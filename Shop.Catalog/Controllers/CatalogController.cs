5b23e3027a9c0b79d5f6c6db4c365745408892ca
Georgios Klaros <geoklar@gmail.com>
2024-08-18 10:19:06 +0300

    Add authorization attribute to the controller

--- Shop.Catalog/Controllers/CatalogController.cs
+++ Shop.Catalog/Controllers/CatalogController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,7 @@ namespace Shop.Catalog.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class CatalogController : ControllerBase
     {
         private readonly CatalogContext _context;
